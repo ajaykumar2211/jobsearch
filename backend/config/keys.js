ec8786c2d6ba5c9ae8fa59669f8f9ce092b2ae71
ajaykumar2211 <126705018+ajaykumar2211@users.noreply.github.com>
2023-05-19 16:14:56 +0530

    Update keys.js

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,4 +1,4 @@
-dbPassword='mongodb+srv://suhani1230:Suhani1230@cluster0.qcod3.mongodb.net/test?retryWrites=true&w=majority'
+dbPassword='mongodb+srv://ajaykumark8489:ajaykumark8489@cluster0.pssmgeu.mongodb.net/?retryWrites=true&w=majority'
 
 module.exports = {
   mongoURI: dbPassword
